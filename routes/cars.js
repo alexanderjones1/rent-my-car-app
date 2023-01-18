72370b18acfe68c92f31fc32a0bd5ca80d9d4fcf
alex-jones <abjhoops@gmail.com>
2023-01-17 19:47:40 -0700

    fill out add new car form and basic style

--- routes/cars.js
+++ routes/cars.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import * as carsCtrl from '../controllers/cars.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
-router.get('/new', carsCtrl.new)
+router.get('/new', isLoggedIn, carsCtrl.new)
 router.post('/', carsCtrl.create)
 
 export {
